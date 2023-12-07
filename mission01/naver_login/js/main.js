756b9777c2b6dd8aea5f949c8fbfabecba6dc7b1
asdfqaz74 <74591618+asdfqaz74@users.noreply.github.com>
2023-12-07 10:27:36 +0900

    Input 에 EventListener 추가

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -1,8 +1,10 @@
+// 공백 또는 유효할 때는 뜨고
+// 뭔가를 입력할 때는 유효하지 않고
 
 const user = {
-  id:'asd@naver.com',
-  pw:'spdlqj123!@'
-}
+  id: "asd@naver.com",
+  pw: "spdlqj123!@",
+};
 
 /*
 
@@ -13,24 +15,68 @@ const user = {
 
 */
 
-function emailReg(text){
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+function emailReg(text) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  return re.test(String(text).toLowerCase())
+  return re.test(String(text).toLowerCase());
 }
 
-function pwReg(text){
+function pwReg(text) {
   const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
   return re.test(String(text).toLowerCase());
 }
 
+// 시작
 
+function validEmail() {
+  // emailInput 에 이메일 input 을 할당
+  const emailInput = document.querySelector("#userEmail");
 
+  // input 에 EventListener 추가
+  emailInput.addEventListener("input", function () {
+    // emailReg 를 통한 input 값을 유효성 검사
+    // isValid 에 true 또는 false 값 할당
+    const isValidEmail = emailReg(emailInput.value);
 
+    // 만약 emailinput.value 가 공백 또는 isValid 가 true 일 때
+    if (emailInput.value == "" || isValidEmail) {
+      // emailInvalid 의 클래스 'is-invalid' 를 제거
+      emailInput.classList.remove("is--invalid");
+    }
+    // isValid 가 false 이면
+    else {
+      // is--invalid 를 추가
+      emailInput.classList.add("is--invalid");
+    }
+  });
+}
 
+validEmail();
 
+function validPassWord() {
+  // passWordInput 에 패스워드 input 을 할당
+  let passWordInput = document.querySelector("#userPassword");
 
+  // input 에 EventListener 추가
+  passWordInput.addEventListener("input", function () {
+    // pwReg 를 통한 input 값을 유효성 검사
+    // isValid 에 true 또는 false 값 할당
+    let isValidPassWord = pwReg(passWordInput.value);
 
+    // 만약 passWordInput.value 가 공백 또는 isValid 가 true 일 때
+    if (passWordInput.value == "" || isValidPassWord) {
+      // passWordInvalid 의 클래스 'is-invalid' 를 제거
+      passWordInput.classList.remove("is--invalid");
+    }
+    // isValid 가 false 이면
+    else {
+      // is--invalid 를 추가
+      passWordInput.classList.add("is--invalid");
+    }
+  });
+}
 
+validPassWord();
 
-
+function isMatch(id, pw) {}
