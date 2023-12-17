6094ea1aad1c12976398ebd70edf9ec555323695
asdfqaz74 <74591618+asdfqaz74@users.noreply.github.com>
2023-12-17 21:54:21 +0900

    refactoring

--- mission02/client/js/main.js
+++ mission02/client/js/main.js
@@ -1,13 +1,3 @@
-/* 
-
-1. 클릭 이벤트 활성화
-2. nav 클릭시 배경 색상 변경
-3. 이미지 변경
-4. 텍스트 변경
-5. 함수 분리
-
-*/
-
 const nav = getNode(".nav");
 const ul = getNode("ul");
 const visualImage = getNode(".visual img");
@@ -15,21 +5,15 @@ const nickName = getNode(".nickName");
 const bodyColor = getNode("body");
 let audioPlayer = new AudioPlayer();
 
-// console.log(nav);
-
-// console.log(data[1]);
-
 function handleClick(e) {
   e.preventDefault();
 
-  let li = e.target.closest("li");
+  const li = e.target.closest("li");
   if (!li) return;
 
-  let index = li.dataset.index;
-  // console.log(index);
+  const index = li.dataset.index;
 
   const list = Array.from(ul.children);
-  // console.log(visualImage);
 
   list.forEach((li) => li.classList.remove("is-active"));
 
@@ -44,12 +28,10 @@ function handleClick(e) {
     `linear-gradient(to bottom,${data[index - 1].color})`
   );
 
-  // audioPlayer
   audioPlayer.stop();
   audioPlayer = new AudioPlayer(`./assets/audio/${data[index - 1].audio}`);
   audioPlayer.loopPlay();
 
-  // console.log(setCss);
   addClass(li, "is-active");
 }
 
